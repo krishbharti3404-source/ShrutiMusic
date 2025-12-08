e0d13ec2d9b655dba70d0f2234ce91a4726454f1
krishbharti3404-source <krishbharti3404@gmail.com>
2025-12-08 11:27:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -112,12 +112,12 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # 🖼️ Image URLs (Can be customized)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/ta84vv.jpg")
+PING_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/ta84vv.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/ta84vv.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
