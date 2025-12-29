a45a856854457e5210bc9a9b3634d275633fad04
krishbharti3404-source <krishbharti3404@gmail.com>
2025-12-29 17:50:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,12 +17,12 @@ LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusic")
+UPSTREAM_REPO = os.getenv("https://github.com/krishbharti3404-source/ShrutiMusic")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/iScamz")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/kryshupdate")
 INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
 YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
 GITHUB = os.getenv("GITHUB", "https://github.com/NoxxOP")
@@ -48,17 +48,17 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 START_STICKER_ENABLED = bool(os.getenv("START_STICKER_ENABLED", "True").lower() in ["true", "1", "yes"])
 
 START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+PING_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/ta84vv.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/ta84vv.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/ta84vv.jpg"
 
 BANNED_USERS = filters.user()
 adminlist = {}
