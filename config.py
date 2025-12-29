2c5f92f87d2718fabc166dbd735097f62bb7bda5
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-12-29 23:22:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,8 @@ LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/nonsecularman/ShrutiMusic",
-)
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/nonsecularman/ShrutiMusic")
+
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
